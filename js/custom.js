3ee85cf6816386c86c3e11ed0aedc3766d18b37a
Gabriel <seguragabriel@unbosque.edu.co>
2020-09-22 14:47:05 -0500

    wip sept 22

--- js/custom.js
+++ js/custom.js
@@ -2,13 +2,30 @@
 	
 	$("#tabs" ).tabs( { active: 0 } );	
 	
-	$("#accordion").accordion({ header: ".click", collapsible: true, active: false });
+	$(".accordion").accordion({ header: ".click", collapsible: true, active: false });
 	
 	// -------------
 	// Menu mobile
 	
 	$(".menu_mobile").click(function() {
 		$("header .first_fold nav ul").toggleClass("open");
+	});
+	
+	// -------------
+	// Dialogs - modals
+	$("section.grupos_apoyo .second_fold .card a.alpec").click(function() {
+		$(".third_fold #alpec").toggleClass("open");
+	});
+	$(".third_fold #alpec img").click(function() {
+		$(".third_fold #alpec").toggleClass("open");
+	});
+	
+	
+	$("section.grupos_apoyo .second_fold .card a.clima").click(function() {
+		$(".third_fold #clima").toggleClass("open");
+	});
+	$(".third_fold #clima img").click(function() {
+		$(".third_fold #clima").toggleClass("open");
 	});
 	
 })(jQuery);
