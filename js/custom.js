d680c6911c273539e38b85a8e251dae9e1c59561
Gabriel <seguragabriel@unbosque.edu.co>
2020-10-02 08:57:22 -0500

    wip oct 2

--- js/custom.js
+++ js/custom.js
@@ -12,28 +12,51 @@
 	});
 	
 	// -------------
-	// Dialogs - modals
+	// Dialogs - modals - alpec
 	$("section.grupos_apoyo .second_fold .card a.alpec").click(function() {
 		$(".third_fold #alpec").toggleClass("open");
+		$(".third_fold #clima").removeClass("open");
+		$(".third_fold #genero").removeClass("open");
+		$(".third_fold #adulto").removeClass("open");
 	});
 	$(".third_fold #alpec img").click(function() {
 		$(".third_fold #alpec").toggleClass("open");
 	});
 	
-	
+	// -------------
+	// Dialogs - modals - clima
 	$("section.grupos_apoyo .second_fold .card a.clima").click(function() {
 		$(".third_fold #clima").toggleClass("open");
+		$(".third_fold #alpec").removeClass("open");
+		$(".third_fold #genero").removeClass("open");
+		$(".third_fold #adulto").removeClass("open");
 	});
 	$(".third_fold #clima img").click(function() {
 		$(".third_fold #clima").toggleClass("open");
 	});
 	
-	
+	// -------------
+	// Dialogs - modals - genero
 	$("section.grupos_apoyo .second_fold .card a.genero").click(function() {
 		$(".third_fold #genero").toggleClass("open");
+		$(".third_fold #clima").removeClass("open");
+		$(".third_fold #alpec").removeClass("open");
+		$(".third_fold #adulto").removeClass("open");
 	});
 	$(".third_fold #genero img").click(function() {
 		$(".third_fold #genero").toggleClass("open");
+	});
+	
+	// -------------
+	// Dialogs - modals - adulto
+	$("section.grupos_apoyo .second_fold .card a.adulto").click(function() {
+		$(".third_fold #adulto").toggleClass("open");
+		$(".third_fold #clima").removeClass("open");
+		$(".third_fold #genero").removeClass("open");
+		$(".third_fold #alpec").removeClass("open");
+	});
+	$(".third_fold #adulto img").click(function() {
+		$(".third_fold #adulto").toggleClass("open");
 	});
 	
 })(jQuery);
